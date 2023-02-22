c703887f6865d8b44c6dc7a17f8b9f0a7909ab39
lena <lheiglauer.mmt-m2021@fh-salzburg.ac.at>
2023-02-22 21:14:44 +0100

    add basic header and include fonts

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <!-- Header -->
+    <header class="flex justify-between items-center px-6 py-4">
+      <NuxtLink to="/">
+        <img src="~/assets/svg/logo-text.svg" alt="FriendsQuest Logo" class="w-32">
+      </NuxtLink>
+      <nav>
+        <ul class="flex">
+          <li>
+            <NuxtLink to="#" class="p-2">What is FriendsQuest?</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="#" class="p-2 mx-4">FAQ</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="#" class="p-2">About Us</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+
+    <slot/>
+
+    <!-- Footer -->
+    <footer>
+
+    </footer>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
+export default {
+    theme: {
+        extend: {
+            colors: {
+                terracotta80: "rgba(175, 78, 33, 0.8)",
+                blue80: "rgba(6, 182, 212, 0.8)"
+            },
+            fontFamily: {
+                sans: ["TexGyreHeros", ...defaultTheme.fontFamily.sans],
+            },
+            fontWeight: {
+                bold: "600"
+            }
+        }
+    }
+}
